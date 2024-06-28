23279d1983e66c56723b3953500dcef0fceb90f9
Micky Fazler <57700946+mickyfazler@users.noreply.github.com>
2024-06-28 12:25:13 +0600

    address screen save the order into db. (END OF PART  4)

--- srcy/screensy/AddressScreeny/index.tsx
+++ srcy/screensy/AddressScreeny/index.tsx
@@ -4,9 +4,27 @@ import {Picker} from '@react-native-picker/picker';
 import countryList from 'country-list';     // https://www.npmjs.com/package/country-list ... working perfect  ... But I don't know why giving errorx...if giving errorx then `npm i --save-dev @types/country-list` or Do whatever vscode suggest 😁
 import Button from '../../componentsy/Buttony';
 import styles from './styles';
+// import {DataStore} from 'aws-amplify';    // old
+import {DataStore} from 'aws-amplify/datastore';
+// import { Auth } from 'aws-amplify';    // his code
+import { getCurrentUser } from 'aws-amplify/auth';
+import {Order,CartProduct,OrderProduct} from '../../../src/models/index.js';
+import {useNavigation} from '@react-navigation/native';
+
 
 // const countryList = require('country-list');
 const countries = countryList.getData();
+// const navigation = useNavigation();    
+/* 
+you will see error like 
+      ` ERROR  Warning: Invalid hook call. Hooks can only be called inside of the body of a function component. This could happen for one of the following reasons:
+    1. You might have mismatching versions of React and the renderer (such as React DOM)
+    2. You might be breaking the Rules of Hooks
+    3. You might have more than one copy of React in the same app
+    See https://reactjs.org/link/invalid-hook-call for tips about how to debug and fix this problem.
+    ERROR  TypeError: Cannot read property 'useContext' of null, js engine: hermes`
+ */ 
+
 
 const AddressScreenbb = () => {
   const [country, setCountry] = useState(countries[18].code);
@@ -19,6 +37,7 @@ const AddressScreenbb = () => {
   const [city, setCity] = useState('');
 
   console.log(fullname);
+const navigation = useNavigation();    
 
   const onCheckout = () => {
     if (addressError) {
@@ -37,6 +56,7 @@ const AddressScreenbb = () => {
     }
 
     console.warn('Success. CHeckout');
+    saveOrderbb()
   };
 
   const validateAddress = () => {
@@ -45,6 +65,59 @@ const AddressScreenbb = () => {
     }
   };
 
+  const saveOrderbb = async () => {
+    console.log('save this order');
+    // get user details
+    // const userData = await Auth.currentAuthenticatedUser();    //old
+    const {  userId } = await getCurrentUser();
+
+    // console.log(userData.attributes.sub);
+    console.log(userId);
+
+    // create a new order in the 'order' table without the products details
+    const newOrderbb = await DataStore.save(
+      new Order({
+        // userSub: userData.attributes.sub,
+        userSub: userId,
+        fullName: fullname,
+        phoneNumber: phone,
+        country,
+        city,
+        address,
+      }),
+    );
+    console.log('newOrderbb--> ',newOrderbb);
+
+    // fetch all OF MY cart items
+            // old way
+              // const cartItemsbb = await DataStore.query(CartProduct, cp =>
+              //   cp.userSub('eq', userData.attributes.sub),
+              // );
+              // GO TO😝-->:https://docs.amplify.aws/gen1/react-native/build-a-backend/more-features/datastore/manipulate-data/#predicates
+    const cartItemsbb = await DataStore.query(CartProduct, (c) => c.userSub.eq(userId));
+      
+
+    // attach all cart items to the order ... we are putting all of our `cartitem` product into the `OrderProduct` table with the previous order `newOrderbb` we did....  automatically all the products with upper 'neworderbb' id will also be update on the 'order' table bb
+    // if you delete any order from `order` table than all the 'orderproduct' will automatically be delete everything own explore: baby
+    await Promise.all(
+      cartItemsbb.map(cartItem =>
+        DataStore.save(
+          new OrderProduct({
+            quantity: cartItem.quantity,
+            option: cartItem.option,
+            productID: cartItem.productID,
+            orderID: newOrderbb.id,
+          }),
+        ),
+      ),
+    );
+
+    // delete all of MY cart items from `CartProduct`
+    await Promise.all(cartItemsbb.map(cartItemy => DataStore.delete(cartItemy)));
+
+    // redirect home
+    navigation.navigate('homeBotTabNm');
+  };
   return (
     <KeyboardAvoidingView     // when we open keyboard to type it should not be hide our box 
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
